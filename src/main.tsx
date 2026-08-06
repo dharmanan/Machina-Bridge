8da7c283f1643d568369b8a5c6dc332b77ba9253
Koray <128939343+dharmanan@users.noreply.github.com>
2026-08-06 23:57:43 +0300

    feat(countdown): expose separate countdown route

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Buffer } from 'buffer'
 import App from './App'
+import CountdownPage from './countdown/CountdownPage'
 import '@rainbow-me/rainbowkit/styles.css'
 import '@mysten/dapp-kit/dist/index.css'
 import { Web3Provider } from './lib/web3'
@@ -15,10 +16,13 @@ const globalScope = globalThis as typeof globalThis & {
 globalScope.Buffer ??= Buffer
 globalScope.global ??= globalThis
 
+const pathname = window.location.pathname.replace(/\/+$/, '') || '/'
+const isCountdownRoute = pathname === '/countdown'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Web3Provider>
-      <App />
+      {isCountdownRoute ? <CountdownPage /> : <App />}
     </Web3Provider>
   </StrictMode>,
 )
